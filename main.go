e6fb0cbfb9ba7a1d7300d2b9d58978341c01ae4f
corvinusz <mz4corvinus@gmail.com>
2017-04-05 01:09:38 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,4 @@
-/*
- * Copyright (c) New Cloud Technologies, Ltd., 2013-2017
- *
- * You can not use the contents of the file in any way without New Cloud Technologies, Ltd. written permission.
- * To obtain such a permit, you should contact New Cloud Technologies, Ltd. at http://ncloudtech.com/contact.html
- *
- */
-
-package main // license/lsx/cmd/lsx-admin-backend/main.go
+package main
 
 import (
 	"flag"
